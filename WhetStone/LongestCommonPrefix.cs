63e60105f264fd95c3152673db9554934aa3c369
Ben <Ben@BEN-PC>
2017-01-31 00:35:16 +0200

    longest common prefix adjustments

--- WhetStone/LongestCommonPrefix.cs
+++ WhetStone/LongestCommonPrefix.cs
@@ -6,12 +6,20 @@ namespace WhetStone.Looping
 {
     public static class longestCommonPrefix
     {
-        public static IEnumerable<IEnumerable<T>> LongestCommonPrefix<T>(this IEnumerable<IEnumerable<T>> @this, IEqualityComparer<T> comp = null)
+        public static IEnumerable<T> LongestCommonPrefix<T>(this IEnumerable<IEnumerable<T>> @this)
+        {
+            return LongestCommonPrefix<T>(@this, null).Select(a=>a.First());
+        }
+        public static IEnumerable<IEnumerable<T>> LongestCommonPrefix<T>(this IEnumerable<IEnumerable<T>> @this, IEqualityComparer<T> comp)
         {
             comp = comp ?? EqualityComparer<T>.Default;
             return @this.Zip().TakeWhile(a => a.AllEqual(comp));
         }
-        public static IEnumerable<Tuple<T, T>> LongestCommonPrefix<T>(this IEnumerable<T> @this, IEnumerable<T> other, IEqualityComparer<T> comp = null)
+        public static IEnumerable<T> LongestCommonPrefix<T>(this IEnumerable<T> @this, IEnumerable<T> other)
+        {
+            return LongestCommonPrefix(@this, other, null).Select(a=>a.Item1);
+        }
+        public static IEnumerable<Tuple<T, T>> LongestCommonPrefix<T>(this IEnumerable<T> @this, IEnumerable<T> other, IEqualityComparer<T> comp)
         {
             comp = comp ?? EqualityComparer<T>.Default;
             return @this.Zip(other).TakeWhile(a => comp.Equals(a.Item1, a.Item2));
