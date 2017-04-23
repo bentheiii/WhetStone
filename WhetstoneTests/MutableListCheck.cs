e1689f29add93eb2445b2449baba1d0f48e6e02a
Ben <Ben@BEN-PC>
2017-04-23 20:35:52 +0300

    generic + wild

--- WhetstoneTests/MutableListCheck.cs
+++ WhetstoneTests/MutableListCheck.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WhetStone.Looping;
@@ -8,7 +10,15 @@ namespace Tests
 {
     internal static class MutableListCheck
     {
+        public static void check(IList<bool> orig, int? seed = null, bool randomSet = true, bool append = true, bool randominsert = true, bool randomRemove = true)
+        {
+            check(orig, a => a.Bool(), seed, randomSet, append, randominsert, randomRemove);
+        }
         public static void check(IList<int> orig, int? seed = null, bool randomSet = true, bool append = true, bool randominsert = true, bool randomRemove = true)
+        {
+            check(orig, a=>a.Int(), seed, randomSet, append, randominsert, randomRemove);
+        }
+        public static void check<T>(IList<T> orig, Func<RandomGenerator, T> create, int? seed = null, bool randomSet = true, bool append = true, bool randominsert = true, bool randomRemove = true, bool wildAction = true)
         {
             int realseed = seed ?? 0;
             RandomGenerator gen;
@@ -22,33 +32,37 @@ namespace Tests
 
             if (randomSet)
             {
-                if(!set(orig, gen))
+                if(!set(orig, gen, create))
                     throw new AssertFailedException("setrandom failed, seed: "+realseed);
             }
             if (append)
             {
-                if (!add(orig, gen))
+                if (!add(orig, gen, create))
                     throw new AssertFailedException("add failed, seed: " + realseed);
             }
             if (randominsert)
             {
-                if (!insert(orig, gen))
+                if (!insert(orig, gen, create))
                     throw new AssertFailedException("insert failed, seed: " + realseed);
+            }
+            if (wildAction)
+            {
+                if (!wild(orig,gen, create))
+                    throw new AssertFailedException("wild failed, seed: " + realseed);
             }
             if (randomRemove)
             {
                 if (!remove(orig, gen))
-                    throw new AssertFailedException("add failed, seed: " + realseed);
+                    throw new AssertFailedException("remove failed, seed: " + realseed);
             }
         }
-
-        private static bool set(IList<int> orig, RandomGenerator gen)
+        private static bool set<T>(IList<T> orig, RandomGenerator gen, Func<RandomGenerator, T> createval)
         {
             var check = orig.ToList();
 
             foreach (int _ in range.Range(100))
             {
-                int val = gen.Int();
+                var val = createval(gen);
                 int ind = gen.Int(orig.Count);
 
                 check[ind] = val;
@@ -59,13 +73,13 @@ namespace Tests
 
             return true;
         }
-        private static bool add(IList<int> orig, RandomGenerator gen)
+        private static bool add<T>(IList<T> orig, RandomGenerator gen, Func<RandomGenerator, T> create)
         {
             var check = orig.ToList();
 
             foreach (int _ in range.Range(100))
             {
-                int val = gen.Int();
+                var val = create(gen);
 
                 check.Add(val);
                 orig.Add(val);
@@ -75,13 +89,13 @@ namespace Tests
 
             return true;
         }
-        private static bool insert(IList<int> orig, RandomGenerator gen)
+        private static bool insert<T>(IList<T> orig, RandomGenerator gen, Func<RandomGenerator, T> create)
         {
             var check = orig.ToList();
 
             foreach (int _ in range.Range(100))
             {
-                int val = gen.Int();
+                var val = create(gen);
                 int ind = gen.Int(orig.Count);
 
                 check.Insert(ind, val);
@@ -92,7 +106,7 @@ namespace Tests
 
             return true;
         }
-        private static bool remove(IList<int> orig, RandomGenerator gen)
+        private static bool remove<T>(IList<T> orig, RandomGenerator gen)
         {
             var check = orig.ToList();
 
@@ -102,6 +116,45 @@ namespace Tests
 
                 check.RemoveAt(ind);
                 orig.RemoveAt(ind);
+                if (!check.SequenceEqualIndices(orig))
+                    return false;
+            }
+
+            return true;
+        }
+        private static bool wild<T>(IList<T> orig, RandomGenerator gen, Func<RandomGenerator, T> create)
+        {
+            var check = orig.ToList();
+
+            foreach (int _ in range.Range(100))
+            {
+                var val = create(gen);
+                int ind = gen.Int(orig.Count);
+
+                Action<IList<T>> act;
+
+                var actKind = gen.Int(4);
+
+                switch (actKind)
+                {
+                    case 0:
+                        act = x => x[ind] = val;
+                        break;
+                    case 1:
+                        act = x => x.Add(val);
+                        break;
+                    case 2:
+                        act = x => x.Insert(ind, val);
+                        break;
+                    case 3:
+                        act = x => x.RemoveAt(ind);
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+                act(check);
+                act(orig);
                 if (!check.SequenceEqualIndices(orig))
                     return false;
             }
